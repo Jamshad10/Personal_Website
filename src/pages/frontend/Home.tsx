0010561d35bc1bbf97ba447b7157bc7d43333ba9
Jamshad <123630640+Jamshad10@users.noreply.github.com>
2023-10-28 16:40:18 +0530

    Animation implimented

--- src/pages/frontend/Home.tsx
+++ src/pages/frontend/Home.tsx
@@ -14,14 +14,14 @@ const Home = () => {
         <img
           src={MAIN_IMG}
           alt="profile image"
-          className=' rounded-full w-80 h-80 lg:w-96 lg:h-96 lg:mb-44'
+          className=' rounded-full w-80 h-80 lg:w-96 lg:h-96 lg:mb-44 animate-fade-down animate-ease-in'
         />
         <div className=" max-w-2xl px-2 py-10 sm:py-10 lg:py-10 lg:ml-16">
           <div className="text-center ">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl animate-fade-right animate-ease-in">
               Hello..
             </h1>
-            <div className="sm:flex sm:justify-center py-4">
+            <div className="sm:flex sm:justify-center py-4 animate-fade-down animate-ease-in">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 I'm a MERN stack developer{' '}
                 <Link to={'https://www.mongodb.com/mern-stack'} className="font-semibold text-indigo-600">
@@ -30,13 +30,13 @@ const Home = () => {
                 </Link>
               </div>
             </div>
-            <h3 className='text-2xl font-semibold'>A Bit About Me</h3>
-            <p className="mt-6 text-lg leading-8 text-gray-600 lg:text-justify">
+            <h3 className='text-2xl font-semibold animate-fade-down animate-ease-in'>A Bit About Me</h3>
+            <p className="mt-6 text-lg text-gray-600 lg:text-justify animate-fade-down animate-ease-in">
               I specialize in crafting dynamic and responsive web applications using MongoDB, Express.js, React, and Node.js.
               With expertise in front-end design and back-end development, I bring modern web solutions to life.
               Let's explore my portfolio to see some of my work!
             </p>
-            <div className="mt-5 flex items-center justify-center lg:gap-x-4 gap-x-2">
+            <div className="mt-5 flex items-center justify-center lg:gap-x-4 gap-x-2 animate-fade-down animate-ease-in">
               {/* instagram */}
               <button
                 type="button"
@@ -127,7 +127,7 @@ const Home = () => {
                 </svg>
               </button>
             </div>
-            <div className='flex justify-around lg:mx-32 mt-4'>
+            <div className='flex justify-around lg:mx-32 mt-4 animate-fade-down animate-ease-in'>
               <div className='flex items-center justify-center w-24 h-24 lg:mr-2 lg:w-32 lg:h-32 my-6 rounded-full bg-yellow-400 text-black border border-gray-800 hover:cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gray-900 hover:text-white duration-300'>
                 <h1 className='text-center text-lg font-medium'>Resume</h1>
               </div>
